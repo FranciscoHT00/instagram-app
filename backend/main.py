4571a8bbea852cd43f63b70b2b70d6851ca0e519
FranciscoHT <franz140000@gmail.com>
2024-06-03 13:25:58 -0500

    Commit Inicial

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/", include_in_schema=False)
+async def documentacion():
+    return RedirectResponse(url="/docs")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",port=8000,reload=True)
